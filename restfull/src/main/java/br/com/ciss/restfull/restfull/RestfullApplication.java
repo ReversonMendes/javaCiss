1762e0fad3b3da6fb47dd7b8e0d25acd396c9116
Reverson <reverson_sk@hotmail.com>
2017-05-15 20:00:51 -0300

    restfullia

---
+++ restfull/src/main/java/br/com/ciss/restfull/restfull/RestfullApplication.java
@@ -0,0 +1,12 @@
+package br.com.ciss.restfull.restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullApplication.class, args);
+	}
+}
