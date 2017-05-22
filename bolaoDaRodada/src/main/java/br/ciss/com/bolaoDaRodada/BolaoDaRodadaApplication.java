998d354d4eaac5ba49d919aff1ec35e6c3a44610
Reverson <reverson_sk@hotmail.com>
2017-05-22 20:44:33 -0300

    bolao

---
+++ bolaoDaRodada/src/main/java/br/ciss/com/bolaoDaRodada/BolaoDaRodadaApplication.java
@@ -0,0 +1,12 @@
+package br.ciss.com.bolaoDaRodada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaoDaRodadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaoDaRodadaApplication.class, args);
+	}
+}
